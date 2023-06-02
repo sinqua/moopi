6dadd55edb0096cbdd5a98c84383a721c674f30c
신규화 <quashine@icloud.com>
2023-06-02 19:17:32 +0900

    next_auth schema is service key

--- lib/database.ts
+++ lib/database.ts
@@ -9,7 +9,7 @@ const options = {
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL as string,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
+  process.env.SUPABASE_SERVICE_ROLE_KEY as string,
   options
 )
 
