7eff2a5cf3cb09ef682089c469b14ec2ebee2f6e
신규화 <quashine@icloud.com>
2023-06-17 15:46:03 +0900

    nextauth error2

--- app/api/join/route.ts
+++ app/api/join/route.ts
@@ -1,22 +1,20 @@
 import { NextRequest, NextResponse } from "next/server";
 import { supabaseAuth, supabase } from "@/lib/database";
-import { getServerSession } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]/route";
 
 export async function POST(req: NextRequest, context: { params: any }) {
-    const json = await req.json();
+  const json = await req.json();
 
-    const { data, error } = await supabaseAuth
-                                .from('users')
-                                .update({'nickname': json.nickname})
-                                .eq('id', json.id )
-                                .select();
+  const { data, error } = await supabaseAuth
+    .from("users")
+    .update({ nickname: json.nickname })
+    .eq("id", json.id)
+    .select();
 
-    return NextResponse.json({
-        status: 200,
-        body: {"user" : data![0]},
-        headers: {
-            "content-type": "text/plain",
-        },
-    });
+  return NextResponse.json({
+    status: 200,
+    body: { user: data![0] },
+    headers: {
+      "content-type": "text/plain",
+    },
+  });
 }
