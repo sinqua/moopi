f2eb12ff6ba4259176efa84e4b67a31266a316ae
신규화 <quashine@icloud.com>
2023-06-17 11:30:19 +0900

    call supabase directly at server component

--- app/user/[id]/page.tsx
+++ app/user/[id]/page.tsx
@@ -4,6 +4,7 @@ import { QuillDeltaToHtmlConverter } from "quill-delta-to-html";
 import Avatar from "@/components/user/Avatar";
 import ProfileCard from "@/components/user/ProfileCard";
 import TabBar from "@/components/user/TabBar";
+import { supabase, supabaseAuth } from "@/lib/database";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const nickname = await getUserNickname(params.id);
@@ -11,10 +12,10 @@ export default async function Page({ params }: { params: { id: string } }) {
   const profile = await getUserProfile(params.id);
   const detail = await getUserDetail(params.id);
 
-  const tags = profile.body.user.tags.map((tag: any) => {
+  const tags = profile.tags.map((tag: any) => {
     return tag.tag;
   });
-  const descriptionObject = JSON.parse(detail.body.detail.description);
+  const descriptionObject = JSON.parse(detail.description);
   const description = await CreateHtml(descriptionObject);
 
   return (
@@ -22,15 +23,15 @@ export default async function Page({ params }: { params: { id: string } }) {
       <div className="flex md:flex-row flex-col justify-center w-full max-w-[1920px] sm:pt-[50px] pt-[20px] md:pb-[60px] md:space-x-[16px] md:space-y-0 sm:space-y-[40px] space-y-[30px]">
         <Avatar IframeUrl={IframeUrl} />
         <ProfileCard
-          profileImage={profileImage.body.auth.image}
-          nickname={nickname.body.user.nickname}
-          description={profile.body.user.description}
+          profileImage={profileImage.image}
+          nickname={nickname.nickname}
+          description={profile.description}
           tags={tags}
-          profile={profile.body.user}
+          profile={profile}
           id={params.id}
         />
       </div>
-      <TabBar description={description}/>
+      <TabBar description={description} />
     </div>
   );
 }
@@ -38,43 +39,48 @@ export default async function Page({ params }: { params: { id: string } }) {
 const IframeUrl = `${process.env.NEXT_PUBLIC_WEBSITE}/threejs`;
 
 const getUserNickname = async (id: string) => {
-  const res = await fetch(process.env.NEXT_PUBLIC_WEBSITE + "/api/user/nickname", {
-    method: "POST",
-    body: JSON.stringify({
-      user_id: id,
-    }),
-  });
-  return res.json();
-};
+  const { data, error } = await supabaseAuth
+    .from("users")
+    .select()
+    .eq("id", id);
 
-const getUserProfileImage = async (id: string) => {
-  const res = await fetch(process.env.NEXT_PUBLIC_WEBSITE + "/api/user/image", {
-    method: "POST",
-    body: JSON.stringify({
-      user_id: id,
-    }),
-  });
-  return res.json();
+  return data![0];
 };
 
 const getUserProfile = async (id: string) => {
-  const res = await fetch(process.env.NEXT_PUBLIC_WEBSITE + "/api/user/profile", {
-    method: "POST",
-    body: JSON.stringify({
-      user_id: id,
-    }),
-  });
-  return res.json();
+  const { data, error } = await supabase
+    .from("profiles")
+    .select(`id, description, image, tags (tag)`)
+    .eq("user_id", id);
+
+  console.log("data", data);
+  return data![0];
+};
+
+const getUserProfileImage = async (id: string) => {
+  const { data: profileData, error: error1 } = await supabase
+    .from("profiles")
+    .select(`image`)
+    .eq("user_id", id);
+
+  const { data: authData, error: error2 } = await supabaseAuth
+    .from("users")
+    .select(`image`)
+    .eq("id", id);
+
+  if (profileData![0].image) {
+    return profileData![0];
+  }
+  return authData![0];
 };
 
 const getUserDetail = async (id: string) => {
-  const res = await fetch(process.env.NEXT_PUBLIC_WEBSITE + "/api/user/detail", {
-    method: "POST",
-    body: JSON.stringify({
-      user_id: id,
-    }),
-  });
-  return res.json();
+  const { data, error } = await supabase
+  .from('user_details')
+  .select()
+  .eq('user_id', id);
+
+  return data![0];
 };
 
 const CreateHtml = async (descriptionObject: any) => {
