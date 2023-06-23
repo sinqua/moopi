36f137a5dc72cd3255586c2798ad02dcfa43eabb
신규화 <quashine@icloud.com>
2023-06-23 17:15:22 +0900

    hotfix

--- app/[user]/[content]/layout.tsx
+++ app/[user]/[content]/layout.tsx
@@ -2,7 +2,7 @@ import TabBar from "@/components/user/TabBar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="w-full">
+    <div className="w-full flex flex-col items-center font-sans grow">
       {children}
     </div>
   );
