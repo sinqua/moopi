1dfb242764757100699c27ee3dc878476a9f5ed2
신규화 <quashine@icloud.com>
2023-07-07 15:56:30 +0900

    hotfix

--- app/[user]/(main)/portfolio/page.tsx
+++ app/[user]/(main)/portfolio/page.tsx
@@ -5,89 +5,16 @@ export const revalidate = 0;
 
 export default async function Page(props: any) {
   const { params } = props;
-  const { vrm, animation, thumbnail } = await GetFileName(props.params.avatar);
-
   const portfolio = await getPortfoilo(params.user);
-  const thumbnaillUrl = await CreateImageUrl(props.params.user, thumbnail);
-  const modelUrl = await CreateModelUrl(props.params.user, vrm);
-  const animationUrl = await CreateAnimationUrl(animation);
 
-  return (
-    <>
-      <Portfolio
-        user={params.user}
-        portfolio={portfolio}
-        modelUrl={modelUrl?.signedUrl}
-        animationUrl={animationUrl?.signedUrl}
-        thumbnailUrl={thumbnaillUrl?.signedUrl}
-      />
-    </>
-  );
+  return <Portfolio user={params.user} portfolio={portfolio} />;
 }
 
 const getPortfoilo = async (id: string) => {
   const { data, error } = await supabase
     .from("avatars")
-    .select()
+    .select(`*, animations(*)`)
     .eq("user_id", id);
 
   return data;
 };
-
-async function CreateImageUrl(userId: string, filename: any) {
-  if (process.env.NEXT_PUBLIC_WEBSITE === "http://localhost:3000") {
-    return { signedUrl: undefined };
-  }
-
-  const filepath = `${userId}/${filename}`;
-
-  const { data, error } = await supabase.storage
-    .from("image")
-    .createSignedUrl(filepath, 3600);
-
-  return data;
-}
-
-async function CreateModelUrl(userId: string, filename: any) {
-  if (process.env.NEXT_PUBLIC_WEBSITE === "http://localhost:3000") {
-    return { signedUrl: undefined };
-  }
-  const filepath = `${userId}/${filename}`;
-
-  const { data, error } = await supabase.storage
-    .from("model")
-    .createSignedUrl(filepath, 3600);
-
-  return data;
-}
-
-async function CreateAnimationUrl(animationId: number) {
-  if (process.env.NEXT_PUBLIC_WEBSITE === "http://localhost:3000") {
-    return { signedUrl: undefined };
-  }
-
-  const { data: filename, error: error1 } = await supabase
-    .from("animations")
-    .select("file")
-    .eq("id", animationId);
-
-  const filepath = `${filename![0].file}`;
-
-  const { data, error } = await supabase.storage
-    .from("animation")
-    .createSignedUrl(filepath, 3600);
-
-  return data;
-}
-
-async function GetFileName(avatar: number) {
-  if (process.env.NEXT_PUBLIC_WEBSITE === "http://localhost:3000") {
-    return { vrm: undefined, animation: undefined, thumbnail: undefined };
-  }
-  const { data, error } = await supabase
-    .from("avatars")
-    .select("vrm, animation, thumbnail")
-    .eq("id", avatar);
-
-  return data![0];
-}
--- components/user/Portfolio.tsx
+++ components/user/Portfolio.tsx
@@ -1,56 +1,82 @@
-"use client";
-import { useEffect, useRef, useState } from "react";
-import AvatarModal from "@/components/AvatarModal";
-import { useRouter } from "next/navigation";
 import PortfolioCanvas from "../PortfolioCanvas";
+import { supabase } from "@/lib/database";
 
-interface PortfolioProps {
+export const revalidate = 0;
+interface PortfolioProps
+ {
   user: any;
   portfolio: { [x: string]: any }[] | null;
-  modelUrl: string | undefined;
-  animationUrl: string | undefined;
-  thumbnailUrl: string | undefined;
 }
 
 export default function Portfolio(props: PortfolioProps) {
-  const { user, portfolio, modelUrl, animationUrl, thumbnailUrl } = props;
-
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleMessage = (event: MessageEvent) => {
-      if (event.data.type === "MODAL") {
-        router.push(
-          `/${event.data.message.userId}/avatar/${event.data.message.avatarId}`
-        );
-      }
-    };
-
-    window.addEventListener("message", handleMessage);
-
-    return () => {
-      window.removeEventListener("message", handleMessage);
-    };
-  }, []);
+  const { user, portfolio } = props;
 
   return (
-    <>
-      <div className="ql-editor relative w-full md:w-[1372px] md:!px-[30px] sm:!px-[60px] !px-[30px] sm:!pt-[40px] !pt-[30px] sm:!pb-[80px] !pb-[50px] grow">
-        <div className="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-[16px]">
-          {portfolio!.map((work: any, index: any) => {
-            return (
-              <PortfolioCanvas
-                userId={user}
-                avatarId={work.id}
-                modelUrl={modelUrl}
-                animationUrl={animationUrl}
-                thumbnailUrl={thumbnailUrl}
-                key={index}
-              />
-            );
-          })}
-        </div>
+    <div className="ql-editor relative w-full md:w-[1372px] md:!px-[30px] sm:!px-[60px] !px-[30px] sm:!pt-[40px] !pt-[30px] sm:!pb-[80px] !pb-[50px] grow">
+      <div className="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-[16px]">
+        {portfolio!.map(async (work: any, index: any) => {
+          const modelUrl = await CreateModelUrl(work.user_id, work.vrm);
+          const animationUrl = await CreateAnimationUrl(work.animation);
+          const thumbnailUrl = await CreateImageUrl(work.user_id, work.thumbnail);
+
+          return (
+            <PortfolioCanvas
+              userId={user}
+              avatarId={work.id}
+              modelUrl={modelUrl?.signedUrl}
+              animationUrl={animationUrl?.signedUrl}
+              thumbnailUrl={thumbnailUrl?.signedUrl}
+              key={index}
+            />
+          );
+        })}
       </div>
-    </>
+    </div>
   );
+}
+
+async function CreateImageUrl(userId: string, filename: any) {
+  if (process.env.NEXT_PUBLIC_WEBSITE === "http://localhost:3000") {
+    return { signedUrl: undefined };
+  }
+
+  const filepath = `${userId}/${filename}`;
+
+  const { data, error } = await supabase.storage
+    .from("image")
+    .createSignedUrl(filepath, 3600);
+
+  return data;
+}
+
+async function CreateModelUrl(userId: string, filename: any) {
+  if (process.env.NEXT_PUBLIC_WEBSITE === "http://localhost:3000") {
+    return { signedUrl: undefined };
+  }
+  const filepath = `${userId}/${filename}`;
+
+  const { data, error } = await supabase.storage
+    .from("model")
+    .createSignedUrl(filepath, 3600);
+
+  return data;
+}
+
+async function CreateAnimationUrl(animationId: number) {
+  if (process.env.NEXT_PUBLIC_WEBSITE === "http://localhost:3000") {
+    return { signedUrl: undefined };
+  }
+
+  const { data: filename, error: error1 } = await supabase
+    .from("animations")
+    .select("file")
+    .eq("id", animationId);
+
+  const filepath = `${filename![0].file}`;
+
+  const { data, error } = await supabase.storage
+    .from("animation")
+    .createSignedUrl(filepath, 3600);
+
+  return data;
 }
