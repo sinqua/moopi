a5573f278b85452e8ffeda42740de8b2e116cba0
신규화 <quashine@icloud.com>
2023-07-13 12:05:16 +0900

    hotfix

--- components/user/ProfileCard.tsx
+++ components/user/ProfileCard.tsx
@@ -63,7 +63,7 @@ export default function ProfileCard(props: ProfileCardProps) {
   return (
     <div className="relative md:w-[482px] md:h-[526px] h-auto sm:p-[30px] sm:pb-[20px] p-[20px] pb-[20px] flex flex-col md:rounded-[10px] rounded-none overflow-hidden shadow-[0px_3px_10px_rgba(0,0,0,0.16)]">
       <div className="flex flex-row md:space-x-[20px] sm:space-x-[30px] space-x-[20px] mb-[30px] relative">
-        {hasImage ? (
+        {/* {hasImage ? (
           <Image
             src={profileImage}
             width={100}
@@ -80,7 +80,15 @@ export default function ProfileCard(props: ProfileCardProps) {
             className="h-[100px] w-[100px] rounded-full border-none"
             alt=""
           />
-        )}
+        )} */}
+        <Image
+            loader={supabasePublicImageLoader}
+            src={`profile-image/${profileImage}`}
+            width={100}
+            height={100}
+            className="h-[100px] w-[100px] rounded-full border-none"
+            alt=""
+          />
         <div className="flex flex-col justify-center space-y-[25px] grow">
           <p className="font-semibold text-[18px]">{nickname ?? ""}</p>
           <div className="flex flex-row text-[14px] flex-wrap md:justify-between md:space-x-0 sm:space-x-[50px] space-x-[30px]">
