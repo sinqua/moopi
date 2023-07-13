3676cc7c05cc76af04730d041c75ce7c344ff4ea
신규화 <quashine@icloud.com>
2023-07-13 12:08:24 +0900

    hotfix

--- components/user/ProfileCard.tsx
+++ components/user/ProfileCard.tsx
@@ -83,7 +83,7 @@ export default function ProfileCard(props: ProfileCardProps) {
         )} */}
         <Image
             loader={supabasePublicImageLoader}
-            src={`profile-image/${profileImage}`}
+            src={`profile-image/${profile.image}`}
             width={100}
             height={100}
             className="h-[100px] w-[100px] rounded-full border-none"
