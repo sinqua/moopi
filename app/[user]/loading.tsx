02786d987271733e436811997e29f6248723b6ee
신규화 <quashine@icloud.com>
2023-07-13 16:48:43 +0900

    hotfix

--- app/[user]/loading.tsx
+++ app/[user]/loading.tsx
@@ -1,7 +1,19 @@
 "use client";
+import { useEffect } from "react";
 import ClipLoader from "react-spinners/ClipLoader";
+import { useSession, signOut } from "next-auth/react";
 
 export default function Loading() {
+
+  
+  const { data: session, status } = useSession();
+
+
+  useEffect(() => {
+    console.log("session", session);
+    console.log("status", status);
+  }, [session, status]);
+
   return (
     <div className="flex h-screen items-center align-middle">
       <ClipLoader color={"#2778C7"} loading={true} size={50} />
