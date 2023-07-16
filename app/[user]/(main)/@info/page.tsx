a1c3e158c0c69c1503a8fc6a1a5b3ab172b28582
신규화 <sinq0216@gmail.com>
2023-07-16 20:26:15 +0900

    hotfix

---
+++ app/[user]/(main)/@info/page.tsx
@@ -0,0 +1,24 @@
+import Description from "@/components/user/Description";
+import { supabase } from "@/lib/database";
+
+export default async function Page({ params }: { params: { user: string } }) {
+  const detail = await getUserDetail(params.user);
+  const descriptionObject =
+    detail.description && JSON.parse(detail.description);
+
+  return <Description descriptionObject={descriptionObject} />;
+}
+
+const getUserDetail = async (id: string) => {
+  const { data, error } = await supabase
+    .from("user_details")
+    .select()
+    .eq("user_id", id)
+    .limit(1)
+    .single();
+
+  if (data) return data;
+  else {
+    throw new Error("User not found");
+  }
+};
