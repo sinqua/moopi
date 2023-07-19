ad311cd26fd05e313c8ad4f09cbb79727f529297
신규화 <quashine@icloud.com>
2023-07-19 11:16:33 +0900

    hotfix

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,6 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import { redirect } from "next/navigation";
 
-export const runtime = 'edge'; 
-
 export default async function Home() {
   const session = await getServerSession(authOptions);
 
