211d8cd83a1b9486ba3a58ec61cba1102e4472e5
신규화 <quashine@icloud.com>
2023-07-20 17:32:04 +0900

    create html at server componenet...

--- app/[user]/(main)/@info/page.tsx
+++ app/[user]/(main)/@info/page.tsx
@@ -1,13 +1,15 @@
 import Description from "@/components/user/info/Description";
 import { supabase } from "@/lib/database";
+import { CreateImageUrl, CreateQuillUrl } from "@/lib/storage";
+import { QuillDeltaToHtmlConverter } from "quill-delta-to-html";
 
 export default async function Page({ params }: { params: { user: string } }) {
   const detail = await getUserDetail(params.user);
-  console.log(detail);
   const descriptionObject =
     detail.description && JSON.parse(detail.description);
+  const description = await CreateHtml(descriptionObject);
 
-  return <Description descriptionObject={descriptionObject} />;
+  return <Description description={description} />;
 }
 
 const getUserDetail = async (id: string) => {
@@ -22,4 +24,35 @@ const getUserDetail = async (id: string) => {
   else {
     throw new Error("User not found");
   }
+};
+
+const CreateHtml = async (descriptionObject: any) => {
+  if (!descriptionObject) return;
+
+  const arr: any[] = [];
+  Object.keys(descriptionObject).forEach((key) =>
+    arr.push(descriptionObject[key])
+  );
+
+  for (let i = 0; i < arr.length; i++) {
+    if (Object.keys(arr[i].insert).includes("image")) {
+      await CreateQuillUrl(arr[i].insert.image).then(async (url) => {
+        arr[i].insert.image = url!.publicUrl;
+        arr[i].attributes = {
+          display: "inline-block",
+          renderAsBlock: true,
+        };
+      });
+    }
+  }
+
+  const converter = new QuillDeltaToHtmlConverter(arr);
+
+  converter.afterRender((groupType: any, htmlString: string) => {
+    htmlString = htmlString.replace(/<img/g, '<img loading="lazy"');
+    return htmlString;
+  });
+
+  const html = converter.convert();
+  return html;
 };
--- components/user/info/Description.tsx
+++ components/user/info/Description.tsx
@@ -1,26 +1,13 @@
 'use client'
 import { useEffect, useState } from 'react';
 import parse from "html-react-parser";
-import { CreateImageUrl, CreateQuillUrl } from "@/lib/storage";
-import { QuillDeltaToHtmlConverter } from "quill-delta-to-html";
 
 interface DescriptionProps {
-  descriptionObject: string | undefined;
+  description: string | undefined;
 }
 
 export default function Description(props: DescriptionProps) {
-  const { descriptionObject } = props;
-
-  const [description, setDescription] = useState<any>(null);
-
-  useEffect(() => {
-    CreateHtml(descriptionObject).then((html) => setDescription(html));
-
-    return () => {
-      setDescription(null);
-    }
-  }, [descriptionObject]);
-
+  const { description } = props;
 
   return (
     <div className="ql-editor relative w-full md:w-[1372px] md:!px-[30px] sm:!px-[60px] !px-[30px] sm:!pt-[40px] !pt-[30px] sm:!pb-[80px] !pb-[50px] grow">
@@ -29,33 +16,3 @@ export default function Description(props: DescriptionProps) {
   );
 }
 
-const CreateHtml = async (descriptionObject: any) => {
-  if (!descriptionObject) return;
-
-  const arr: any[] = [];
-  Object.keys(descriptionObject).forEach((key) =>
-    arr.push(descriptionObject[key])
-  );
-
-  for (let i = 0; i < arr.length; i++) {
-    if (Object.keys(arr[i].insert).includes("image")) {
-      await CreateQuillUrl(arr[i].insert.image).then(async (url) => {
-        arr[i].insert.image = url!.publicUrl;
-        arr[i].attributes = {
-          display: "inline-block",
-          renderAsBlock: true,
-        };
-      });
-    }
-  }
-
-  const converter = new QuillDeltaToHtmlConverter(arr);
-
-  converter.afterRender((groupType: any, htmlString: string) => {
-    htmlString = htmlString.replace(/<img/g, '<img loading="lazy"');
-    return htmlString;
-  });
-
-  const html = converter.convert();
-  return html;
-};
