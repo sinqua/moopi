79c9dfdecc12da6c0be8b0f8686b8aada106b2f8
신규화 <quashine@icloud.com>
2023-07-25 11:50:50 +0900

    set default value

--- components/edit/profileCard/description.tsx
+++ components/edit/profileCard/description.tsx
@@ -1,30 +1,41 @@
-'use client'
+"use client";
 import { useEffect, useRef, useState } from "react";
 
 interface DescriptionProps {
-	session: any;
-    profile: any;
-    inputDescriptionRef: any;
+  session: any;
+  profile: any;
+  inputDescriptionRef: any;
 }
 
 export const Description = (props: DescriptionProps) => {
-    const { session, profile, inputDescriptionRef } = props;
+  const { session, profile, inputDescriptionRef } = props;
 
-    const [textareaCount, setTextareaCount] = useState(0);
+  const [textareaCount, setTextareaCount] = useState(0);
 
-    useEffect(() => {
-        inputDescriptionRef.current.value = profile ? profile.description : "";
-        setTextareaCount(inputDescriptionRef.current.value.length);
-    }, [profile]);
+  useEffect(() => {
+    setTextareaCount(inputDescriptionRef.current.value.length);
+  }, [profile]);
 
-    return (
-        <div>
-            <p className="mb-[15px] text-[20px] font-semibold">자기소개</p>
-            <p className="mb-[30px] text-[#7B7B7B] leading-[25px]">매력적인 문구로 회원님을 소개해주세요.</p>
-            <div className="relative sm:w-min w-full">
-                <textarea ref={inputDescriptionRef} className="sm:w-[482px] w-full h-[260px] sm:p-[30px] p-[20px] rounded-[10px] resize-none bg-white border-solid border-[1px] border-[#CCCCCC]" placeholder="자기소개를 입력해주세요." onChange={() => setTextareaCount(inputDescriptionRef.current.value.length)} />
-                <span className="absolute bottom-[20px] sm:right-[30px] right-[20px] text-[#7B7B7B]">{textareaCount} / 200</span>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div>
+      <p className="mb-[15px] text-[20px] font-semibold">자기소개</p>
+      <p className="mb-[30px] text-[#7B7B7B] leading-[25px]">
+        매력적인 문구로 회원님을 소개해주세요.
+      </p>
+      <div className="relative sm:w-min w-full">
+        <textarea
+          ref={inputDescriptionRef}
+          className="sm:w-[482px] w-full h-[260px] sm:p-[30px] p-[20px] rounded-[10px] resize-none bg-white border-solid border-[1px] border-[#CCCCCC]"
+          placeholder="자기소개를 입력해주세요."
+          defaultValue={profile ? profile.description : ""}
+          onChange={() =>
+            setTextareaCount(inputDescriptionRef.current.value.length)
+          }
+        />
+        <span className="absolute bottom-[20px] sm:right-[30px] right-[20px] text-[#7B7B7B]">
+          {textareaCount} / 200
+        </span>
+      </div>
+    </div>
+  );
+};
