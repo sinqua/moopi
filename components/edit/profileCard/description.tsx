ebaf037432ef8af58d7446a6be9a24c392464f94
신규화 <quashine@icloud.com>
2023-07-25 13:01:40 +0900

    no placeholder

--- components/edit/profileCard/description.tsx
+++ components/edit/profileCard/description.tsx
@@ -27,7 +27,7 @@ export const Description = (props: DescriptionProps) => {
         <textarea
           ref={inputDescriptionRef}
           className="sm:w-[482px] w-full h-[260px] sm:p-[30px] p-[20px] rounded-[10px] resize-none bg-white border-solid border-[1px] border-[#CCCCCC]"
-          placeholder="자기소개를 입력해주세요."
+          placeholder=""
           value={profile.description}
           onChange={() =>
             setTextareaCount(inputDescriptionRef.current.value.length)
