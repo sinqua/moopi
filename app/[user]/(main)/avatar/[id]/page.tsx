e826a39773f19ad9d390a723002ceb302f2d9e37
신규화 <quashine@icloud.com>
2023-07-27 13:02:25 +0900

    fix for if not modal

--- app/[user]/(main)/avatar/[id]/page.tsx
+++ app/[user]/(main)/avatar/[id]/page.tsx
@@ -1,128 +1,134 @@
 import React from "react";
 import AvatarModal from "@/components/user/profile/AvatarModal";
 import { supabase, supabaseAuth } from "@/lib/database";
-import { CreateImageUrl } from "@/lib/storage";
 
 export const revalidate = 0;
 
 export default async function Avatar(props: any) {
   const { params } = props;
 
-  const { vrm, animation, thumbnail } = await GetFileName(params.avatar);
-  const modelUrlData = CreateModelUrl(params.user, vrm);
-  const animationUrlData = CreateAnimationUrl(animation!);
-  const profileImageData = getUserProfileImage(params.user);
-  const nicknameData = getUserNickname(params.user);
-  const avatarInfoData = getAvatarInfo(params.id);
+  const profileData = getProfile(params.user);
+  const authData = getAuth(params.user);
+  const avatarData = getAvatar(params.avatar);
+  const portfoliosData = getUserPortfolios(params.user);
 
-  const [modelUrl, animationUrl, profileImage, nickname, avatarInfo] =
-    await Promise.all([
-      modelUrlData,
-      animationUrlData,
-      profileImageData,
-      nicknameData,
-      avatarInfoData,
-    ]);
+  const [
+    profile,
+    auth,
+    avatar,
+    portfolios,
+  ] = await Promise.all([
+    profileData,
+    authData,
+    avatarData,
+    portfoliosData,
+  ]);
 
-  const tags = avatarInfo.tags.map((tag: any) => {
+  const tags = avatar.tags.map((tag: any) => {
     return tag.tag;
   });
+  const modelUrl = await createModelUrl(params.user, avatar?.vrm);
+
 
   return (
     <AvatarModal
-      userId={props.params.user}
-      avatarId={props.params.avatar}
-      profile={profileImage.image}
-      nickname={nickname.nickname}
-      avatarInfo={avatarInfo}
+      profile={profile}
+      nickname={auth.nickname}
+      avatar={avatar}
       tags={tags}
+      portfolios={portfolios}
       modelUrl={modelUrl?.signedUrl}
-      animationUrl={animationUrl?.signedUrl}
-      portfolios={null}
+      animation={avatar?.animation}
     />
   );
 }
 
-const getUserProfileImage = async (id: string) => {
-  const { data: profileData, error: error1 } = await supabase
-    .from("profiles")
-    .select(`image`)
-    .eq("user_id", id);
+const getAvatar = async (id: string) => {
+  const { data, error } = await supabase
+    .from("avatars")
+    .select('*, tags (tag)')
+    .eq("id", id)
+    .limit(1)
+    .single();
 
-  const { data: authData, error: error2 } = await supabaseAuth
-    .from("users")
-    .select(`image`)
-    .eq("id", id);
-
-  if (profileData![0].image) {
-    const url = await CreateImageUrl(profileData![0].image);
-    return { image: url!.signedUrl };
+  if (data) return data;
+  else {
+    throw new Error("Avatar not found");
   }
-  return authData![0];
 };
 
-const getUserNickname = async (id: string) => {
+const getProfile = async (id: string) => {
+  const { data, error } = await supabase
+    .from("profiles")
+    .select(`*,  tags (tag)`)
+    .eq("user_id", id)
+    .limit(1)
+    .single();
+
+  if (data) return data;
+  else {
+    throw new Error("Profile not found");
+  }
+};
+
+const getAuth = async (id: string) => {
   const { data, error } = await supabaseAuth
     .from("users")
     .select()
-    .eq("id", id);
+    .eq("id", id)
+    .limit(1)
+    .single();
 
-  return data![0];
-};
-
-const getAvatarInfo = async (id: string) => {
-  const { data, error } = await supabase
-    .from("avatars")
-    .select(`id, description, name, tags (tag), created_at`)
-    .eq("id", id);
-
-    console.log("getAvatarInfo", data);
-
-  return data![0];
-};
-
-async function CreateModelUrl(userId: string, filename: any) {
-  if (process.env.NEXT_PUBLIC_ENV === "Development") {
-    return { signedUrl: undefined };
+  if (data) return data;
+  else {
+    throw new Error("User not found");
   }
+};
+
+async function createModelUrl(userId: string, filename: any) {
+  if(!filename) return { signedUrl: "" };
+
   const filepath = `${userId}/${filename}`;
 
   const { data, error } = await supabase.storage
     .from("model")
     .createSignedUrl(filepath, 3600);
 
-  return data;
-}
-
-async function CreateAnimationUrl(animationId: number) {
-  if (process.env.NEXT_PUBLIC_ENV === "Development") {
-    return { signedUrl: undefined };
+  if (data) return data;
+  else{
+    throw new Error("Model not found");
   }
-
-  const { data: filename, error: error1 } = await supabase
-    .from("animations")
-    .select("file")
-    .eq("id", animationId);
-
-  const filepath = `${filename![0].file}`;
-
-  const { data, error } = await supabase.storage
-    .from("animation")
-    .createSignedUrl(filepath, 3600);
-
-  return data;
 }
 
-async function GetFileName(avatar: number) {
-  // if (process.env.NEXT_PUBLIC_ENV === "Development") {
-  //   return { vrm: undefined, animation: undefined, thumbnail: undefined };
+const getUserPortfolios = async (id: string) => {
+  // const { data: portfoiloData, error: portfolioError } = await supabase
+  //   .from("avatars")
+  //   .select()
+  //   .eq("user_id", id);
+
+  // const portfolios = [];
+  // for (const portfolio of portfoiloData!) {
+  //   const { data: tagData, error: tagError } = await supabase
+  //     .from("tags")
+  //     .select("tag")
+  //     .eq("avatar_id", portfolio.id);
+
+  //   const tags = tagData?.map((tag: any) => Object.values(tag)[0]) || [];
+  //   portfolio.tags! = tags;
+
+  //   const { data: anmiationData, error: animationError } = await supabase
+  //     .from("animations")
+  //     .select()
+  //     .eq("id", portfolio.animation);
+  //   portfolio.animation = anmiationData![0];
+
+  //   const url = await CreateImageUrl(
+  //     portfolio.user_id + "/" + portfolio.thumbnail
+  //   );
+  //   portfolio.thumbnailUrl = url ? url!.signedUrl : "";
+
+  //   portfolios.push(portfolio);
   // }
-  const { data, error } = await supabase
-    .from("avatars")
-    .select("vrm, animation, thumbnail")
-    .eq("id", avatar);
 
-  console.log("getFileName", data);
-
-  return data![0];
-}
+  // return portfolios;
+};
