9b839e75bae3f1b72ca59047fc64d2c5122b3524
신규화 <quashine@icloud.com>
2023-08-04 18:09:50 +0900

    rewrite header

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           무피는&nbsp;
-          <code className="font-mono font-bold">3D 컨텐츠&nbsp;</code>
-          중심의 크리에이터 플랫폼입니다.
+          <code className="font-mono font-bold">직관적인 3D 컨텐츠&nbsp;</code>
+          업로드 과정을 만들어갑니다.
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
